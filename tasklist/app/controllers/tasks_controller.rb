1d1c6d255659623c6e61250fe8e05567e969836b
naoki.yonemoto <miyukiismiwiki@gmail.com>
2018-02-11 18:03:57 +0000

    CRUD kadai-tasklist

---
+++ tasklist/app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:succes] = "タスクが記入されました。"
+      redirect_to @task
+    else
+      flash.now[:danger] ="タスクが記入されませんでした。"
+      render :new
+    end
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:succes] = "タスクが更新されました。"
+      redirect_to @task
+    else
+      flash.now[:danger] ="タスクが更新されませんでした。"
+      render :new
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:succes] = "タスクが更新されました。"
+    redirect_to tasks_url
+    
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
